345156d6714d69c57f16d7eea2f09bc0993b260c
HTCKCD <linjieli_uestc@163.com>
2021-06-07 19:48:33 +0800

    迷宫问题修改

--- 迷宫问题/MiGong.java
+++ 迷宫问题/MiGong.java
@@ -11,6 +11,10 @@ public class MiGong{
 		}
 		map[3][1] = 1;
 		map[3][2] = 1;
+		map[2][2] = 1;
+		//map[3][3] = 1;
+		//map[3][4] = 1;
+		//map[3][5] = 1;
 		for (int i=0;i<8;i++){
 			for (int j=0;j<7;j++){
 				System.out.print(map[i][j]+" ");
@@ -27,7 +31,7 @@ public class MiGong{
 	}
 	//使用递归回溯算法来给小球找路
 	/**
-	
+	先向下走，后向右->上->左
 	*/
 	public static boolean setWay(int[][] map, int i, int j){
 		if(map[6][5] == 2){
@@ -39,11 +43,14 @@ public class MiGong{
 					return true;
 				}else if(setWay(map, i, j+1)){  
 					return true;
-				}else if(setWay(map, i-1, j)){   
+				}
+				/**
+				else if(setWay(map, i-1, j)){   
 					return true;
 				}else if(setWay(map, i, j-1)){
 					return true;
 				}
+				*/
 				map[i][j]= 3;
 				return false;
 			}else{
